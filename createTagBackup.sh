f3a6f71cea2417cfaffd8675254ee9745556232a
toad <thomasmcdonald2027@gmail.com>
2022-03-06 12:35:28 +0000

    Update createTagBackup.sh

--- createTagBackup.sh
+++ createTagBackup.sh
@@ -1,6 +1,6 @@
 docker build .
 tagName=$(docker images | awk '{print $3}' | awk 'NR==2')
-docker tag $tagName frogg1t/monkeapi:latest
+docker tag $tagName toadlessss/monkeapi:latest
 docker login
-docker push frogg1t/monkeapi:latest
+docker push toadlessss/monkeapi:latest
 echo "Done!"
